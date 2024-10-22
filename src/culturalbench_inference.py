fa50f5e5fd4c00fd59bb319eb18751423ae2d556
benediktstroebl <stroebl@princeton.edu>
2024-10-21 23:22:36 -0400

    Update culturalbench_inference.py

--- src/culturalbench_inference.py
+++ src/culturalbench_inference.py
@@ -21,7 +21,7 @@ client = openai.AsyncAzureOpenAI(
 
 # Backoff on rate limit errors
 @backoff.on_exception(backoff.expo, openai.RateLimitError)
-async def make_api_call_to_gpt(prompt, model="gpt-4o") -> List:
+async def make_api_call_to_gpt(prompt, model) -> List:
     messages = [{"role": "user", "content": prompt}]
     try: 
         response = await client.chat.completions.create(
@@ -37,7 +37,7 @@ async def make_api_call_to_gpt(prompt, model="gpt-4o") -> List:
         return ""
 
 # Function to limit the number of concurrent API calls
-async def limited_api_call(semaphore, prompt, answer, country, q_id, model="gpt-4o") -> List:
+async def limited_api_call(semaphore, prompt, answer, country, q_id, model) -> List:
     async with semaphore:
         return await make_api_call_to_gpt(prompt, model=model), answer, country, q_id
 
@@ -56,8 +56,8 @@ async def main(model, language, include_country):
     dataset = CulturalBenchDataset(lang=LANGUAGE,
                                    include_country=INCLUDE_COUNTRY)
         
-    MODEL_NAME = 'gpt-4o' # "Meta-Llama-3-1-70B-Instruct-htzs", 'GPT-4o'
-    data_dir = f'data/cultural_bench/{MODEL_NAME}'
+    MODEL_NAME = model # "Meta-Llama-3-1-70B-Instruct-htzs", 'GPT-4o'
+    data_dir = f'data/cultural_bench'
 
     data = []
 
@@ -68,7 +68,7 @@ async def main(model, language, include_country):
     tasks = [limited_api_call(semaphore, dataset[i][0], dataset[i][1], dataset[i][2], dataset[i][3], model=MODEL_NAME) for i in range(len(dataset))]
 
     # Gather and run the tasks concurrently
-    results = await tqdm_asyncio.gather(*tasks, total=len(tasks), desc="Processing")
+    results = await tqdm_asyncio.gather(*tasks, total=len(tasks), desc=f"Processing_{LANGUAGE}_{INCLUDE_COUNTRY}_{model}")
 
     # add results to the jsonl file
     for i, result in enumerate(results):
@@ -82,7 +82,7 @@ async def main(model, language, include_country):
 
 
     # Write the data to jsonl file
-    os.makedirs(data_dir, exist_ok=True)
+    os.makedirs(os.path.join(data_dir, LANGUAGE), exist_ok=True)
     country_include = 'with_country' if INCLUDE_COUNTRY else 'no_country'
     with open(f'{data_dir}/{LANGUAGE}/eval_results_{country_include}_{model}.jsonl', 'w') as f:
         for item in data:
@@ -98,6 +98,6 @@ if __name__ == "__main__":
     parser.add_argument('--model', type=str, default='gpt-4o', help='model to use for inference')
     args = parser.parse_args()
 
-    # weave.init(f"culturalbench_inference_{args.lang}_{args.include_country}")
+    # weave.init(f"culturalbench_inference_{args.lang}_{args.include_country}_{args.model}")
 
     asyncio.run(main(args.model, args.lang, args.include_country))
