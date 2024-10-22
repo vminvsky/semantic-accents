9f27c701a544daec92d13877ad354476d592cf03
Veniamin Veselovsky <venia@veselovsky.org>
2024-10-21 22:00:41 -0400

    dataclass for formatting the culturalbench in the correct format

---
+++ src/prompts.py
@@ -0,0 +1,122 @@
+import pandas as pd 
+import os 
+from dataclasses import dataclass, field
+from datasets import Dataset
+
+from utils import open_file
+
+@dataclass 
+class CulturalBenchDataset(Dataset):
+    # TODO: FIGURE OUT HOW TO HANDLE QUESTIONS FROM SPECIFIC COUNTRIES
+    lang: str 
+    difficulty: str = field(default='easy') # 'easy' or 'hard'
+    model_name: str = field(default='gpt-4o')
+    include_country: bool = field(default=False)
+    randomize_options: bool = field(default=False)
+    
+    # dataframe 
+    df: pd.DataFrame = field(default=None)
+    col_mapping: dict = field(default_factory=dict)
+    data_dir: str = field(default=os.path.join('data', 'cultural_bench'))
+    data_path: str = field(default=None)
+    options: list = field(default_factory=lambda: ['A', 'B', 'C', 'D'])
+    drop_nans: bool = field(default=True) # NOTE: THIS IS BAD TRANSLATIONS. FIXXXX!
+
+    prompt_template: str = field(default=None)
+    prompt_template_path: str = field(default=os.path.join(
+                "data", "prompts", "{lang}", "cultural_bench_{difficulty}.txt"))
+    multi_option_str: str = field(default=None)
+    multi_option_str_path: str = field(default=os.path.join(
+                "data", "prompts", "{lang}", "multi_option.txt"))
+
+    def __post_init__(self):
+        if self.randomize_options is True:
+            raise NotImplementedError("Randomizing options is not yet supported")
+        
+        if self.difficulty == 'hard':
+            raise NotImplementedError("Hard difficulty is not yet supported")
+
+        self._create_col_mapping()
+        if self.lang == 'en':
+            self.data_path = os.path.join(self.data_dir, self.model_name, 'no_country.jsonl')
+            
+        else: 
+            self.data_path = os.path.join(self.data_dir, self.model_name, self.lang, f'data.jsonl')
+
+        self.df = pd.read_json(self.data_path, lines=True)
+        self.df.dropna(inplace=True)
+
+        if self.prompt_template is None:
+            self.prompt_template = open_file(self.prompt_template_path.format(lang=self.lang, difficulty=self.difficulty))
+
+        if self.multi_option_str is None:
+            self.multi_option_str = open_file(self.multi_option_str_path.format(lang=self.lang))
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+
+        if self.difficulty == 'easy':
+            prompt = self._easy_formatting(row)
+            return prompt
+
+    def _easy_formatting(self, row):
+        multi_option = False
+        prompt_template = self.prompt_template
+
+        if self.include_country is True:
+            question_col = self.col_mapping['question']
+        else:
+            question_col = self.col_mapping['question_no_country']
+        
+        question = row[question_col]
+        if ('i' in question) & ('ii' in question) & ('iii' in question) & ('iv' in question):
+            multi_option = True
+
+        # if multi_option is True:
+        #     prompt_list = prompt_template.split('\n')
+        #     prompt_list[0] = prompt_list[0] + ' ' + self.multi_option_str
+        #     prompt_template = '\n'.join(prompt_list)
+                
+        question = prompt_template.format(question=question)
+
+        answer = row[self.col_mapping['answer']]
+        country = row[self.col_mapping['country']]
+
+        options = self._return_options(row)
+        prompt = self._add_options(question, options)
+        return prompt, answer, country
+
+    def _return_options(self, row):
+        options = [row[self.col_mapping[f'option_{option}']] for option in ['a', 'b', 'c', 'd']]
+        return options
+    
+    def _add_options(self, prompt, options):
+        return prompt + '\n' + '\n'.join([f"{char}: {option}" for char, option in zip(self.options, options)])
+
+    def _create_col_mapping(self):
+        self.col_mapping = {'country': 'country', 'answer': 'answer'}
+        if self.lang == 'en':
+            self.col_mapping.update({
+                'question_no_country': 'prompt_no_country',
+                'question': 'prompt_question',
+            })
+            for option in ['a', 'b', 'c', 'd']:
+                self.col_mapping[f'option_{option}'] = f'prompt_option_{option}'
+        else: 
+            self.col_mapping.update({
+                'question_no_country': f'prompt_no_country_{self.lang}',
+                'question': f'prompt_question_{self.lang}',
+            })
+            for option in ['a', 'b', 'c', 'd']:
+                self.col_mapping[f'option_{option}'] = f'prompt_option_{option}_{self.lang}'
+
+    def __len__(self):
+        return len(self.df)
+    
+def main():
+    dataset = CulturalBenchDataset(lang='en', include_country=True)
+    for i in range(20):
+        print(dataset[i][0])
+
+if __name__ == "__main__":
+    main()
